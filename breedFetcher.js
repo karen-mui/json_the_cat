0d7f956b17ee3aed11d9572ba0f6a80ca97f2a1b
Karen Mui <karen_mui@hotmail.com>
2022-11-27 00:37:07 +0000

    refactored code, added mocha and chai test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,21 @@
-const args = (process.argv).slice(2);
+
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  } else {
-    try {
-      const data = JSON.parse(body);
-      console.log(data[0]["description"]);
-    } catch (error) {
-      console.log("dat ain't a cat, try again bro");
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     }
-  }
-});
+
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      const errMsg = "that cat doesn't exist!";
+      callback(errMsg, null);
+    } else {
+      callback(null, (data[0]["description"]));
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
